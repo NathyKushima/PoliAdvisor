29b8ca8df2bcf87bfe939a9fbd399750f158fa0a
NathyKushima <nhkushima@gmail.com>
2024-12-06 12:51:25 -0300

    arrumando erros do render - biblioteca

--- PoliAdvisor/settings.py
+++ PoliAdvisor/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -159,7 +160,7 @@ ALLOWED_HOSTS = ['https://poliadvisor.onrender.com']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-import dj_database_url
+
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3'  
