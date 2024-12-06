7958f26b56304e2f3ede426065437b216dbc3f37
NathyKushima <nhkushima@gmail.com>
2024-12-06 13:14:53 -0300

    allowed_origin

--- PoliAdvisor/settings.py
+++ PoliAdvisor/settings.py
@@ -136,13 +136,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+CORS_ALLOW_CREDENTIALS = True 
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    'https://poliadvisor-1.onrender.com',
-    'https://poliadvisor.onrender.com'
-]
-CORS_ALLOW_CREDENTIALS = True 
+    "https://poliadvisor-1.onrender.com",
+    "https://poliadvisor.onrender.com"]
+
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend/public/images')
