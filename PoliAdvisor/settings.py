42a63f3e7f61792eb0f1c76b0a6b55bc5af41dd4
NathyKushima <nhkushima@gmail.com>
2024-12-06 12:59:33 -0300

    allowed_origins

--- PoliAdvisor/settings.py
+++ PoliAdvisor/settings.py
@@ -138,7 +138,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    'https://poliadvisor-1.onrender.com/',
+    'https://poliadvisor.onrender.com/'
 ]
 CORS_ALLOW_CREDENTIALS = True 
 
